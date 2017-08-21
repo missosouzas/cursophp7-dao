316e99a934b15fa77ff945c1c1fe1427670b68b6
Edmilson Alves de Souza <missosouzas@hotmail.com>
2017-08-21 14:47:06 -0400

    estudos DAO

--- index.php
+++ index.php
@@ -2,11 +2,30 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega um usuario
+//$root = new Usuario();
 
-$root->loadbyId(5);
+//$root->loadbyId(5);
 
-echo $root;
+//echo $root;
 
+//carrega uma lista de uusarios
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//carrega uma lista de uusarios buscando pelo login
+
+//$search = Usuario::search("s");
+
+//echo json_encode($search);
+
+//carrega usuario usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("jose","123123123"); 
+
+echo $usuario;
 
 ?>
